ca2dde46271cd40b8e53a0d3b32b9b7601a61400
Adam Celarek <git_commits@xibo.at>
2019-11-26 21:17:21 +0100

    wip: experiments with gm conv module + gm relu

---
+++ prototype/experiments_gm_modules.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+
+import gm_modules
+import gm
+
+m = gm.generate_random_mixtures(n_layers=3, n_components=4, n_dims=2, pos_radius=10, cov_radius=0.25, weight_min=0)
+
+gmc1 = gm_modules.GmConvolution(3, 5, n_kernel_components=6)
+relu1 = gm_modules.GmBiasAndRelu(5, 10)
+gmc2 = gm_modules.GmConvolution(5, 3, n_kernel_components=6)
+relu2 = gm_modules.GmBiasAndRelu(3, 10)
+
+x = m
+print(f"n_layers = {x.n_layers()}, n_components = {x.n_components()}")
+fig, sbs = plt.subplots(1, x.n_layers())
+for i, sb in enumerate(sbs.flat):
+    im = x.debug_show(i, -20, -20, 20, 20, 0.2, imshow=False)
+    im = sb.imshow(im)
+fig.colorbar(im, ax=sbs.ravel().tolist())
+fig.show()
+
+x = gmc1(x)
+print(f"n_layers = {x.n_layers()}, n_components = {x.n_components()}")
+fig, sbs = plt.subplots(1, x.n_layers())
+for i, sb in enumerate(sbs.flat):
+    im = x.debug_show(i, -20, -20, 20, 20, 0.2, imshow=False)
+    im = sb.imshow(im)
+fig.colorbar(im, ax=sbs.ravel().tolist())
+fig.show()
+
+x = relu1(x)
+print(f"n_layers = {x.n_layers()}, n_components = {x.n_components()}")
+fig, sbs = plt.subplots(1, x.n_layers())
+for i, sb in enumerate(sbs.flat):
+    im = x.debug_show(i, -20, -20, 20, 20, 0.2, imshow=False)
+    im = sb.imshow(im)
+fig.colorbar(im, ax=sbs.ravel().tolist())
+fig.show()
+
+x = gmc2(x)
+print(f"n_layers = {x.n_layers()}, n_components = {x.n_components()}")
+fig, sbs = plt.subplots(1, x.n_layers())
+for i, sb in enumerate(sbs.flat):
+    im = x.debug_show(i, -20, -20, 20, 20, 0.2, imshow=False)
+    im = sb.imshow(im)
+fig.colorbar(im, ax=sbs.ravel().tolist())
+fig.show()
+
+x = relu2(x)
+print(f"n_layers = {x.n_layers()}, n_components = {x.n_components()}")
+fig, sbs = plt.subplots(1, x.n_layers())
+for i, sb in enumerate(sbs.flat):
+    im = x.debug_show(i, -20, -20, 20, 20, 0.2, imshow=False)
+    im = sb.imshow(im)
+fig.colorbar(im, ax=sbs.ravel().tolist())
+fig.show()
+
--- prototype/gm_modules.py
+++ prototype/gm_modules.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.modules
 
 import gm
+import gm_fitting
 
 
 class GmConvolution(torch.nn.modules.Module):
@@ -23,6 +24,8 @@ class GmConvolution(torch.nn.modules.Module):
 
         for i in range(self.n_layers_out):
             k = gm.generate_random_mixtures(n_layers_in, n_kernel_components, n_dims, pos_radius=position_range, cov_radius=covariance_range, weight_min=weight_min, weight_max=weight_max)
+            # positive mean produces a rather positive gm. i believe this is a better init
+            k.weights += k.weights.mean(dim=1).view(-1, 1) + 0.2
             self.kernels.append(k)
             self.weights.append(torch.nn.Parameter(k.weights))
             self.positions.append(torch.nn.Parameter(k.positions))
@@ -39,22 +42,26 @@ class GmConvolution(torch.nn.modules.Module):
 
 
 class GmBiasAndRelu(torch.nn.modules.Module):
-    def __init__(self, n_layers: int, n_output_gaussians: int, train_fitting_net: bool = True):
+    def __init__(self, n_layers: int, n_output_gaussians: int = 10, n_dimensions=2, train_fitting_net: bool = False):
+        # todo support variable number of outputs and configurable net archs. needs a better init + training routine (start with few gaussians etc?)
+        assert n_output_gaussians == 10
         super(GmBiasAndRelu, self).__init__()
         self.n_layers = n_layers
         self.n_output_gaussians = n_output_gaussians
         self.train_fitting_net = train_fitting_net
-        self.bias = torch.nn.Parameter(torch.rand(self.n_layers))
+        # use a small bias for the start. i hope it's easier for the net to increase it than to lower it
+        self.bias = torch.nn.Parameter(torch.rand(self.n_layers) * 0.2)
 
-        #todo: load fitting net
-
-        #todo: option to make fitting net have common or seperate weights per module
+        self.net = gm_fitting.Net([64, 128, 128, 512, 512 * n_output_gaussians],
+                                  [512, 256, 128, 64, 32],
+                                  n_output_gaussians=n_output_gaussians,
+                                  n_dims=n_dimensions)
+        self.net.load()
+        # todo: option to make fitting net have common or seperate weights per module
 
     def forward(self, x: gm.Mixture) -> gm.Mixture:
-        x = gm.MixtureReLUandBias(x, self.bias)
-
+        x = gm.MixtureReLUandBias(x, torch.abs(self.bias))
 
         # todo: think of something that would make it possible to do live learning of the fitting network
 
-        # todo: fitting and return
-
+        return self.net(x)
