f4d778e11873ea112560ad21d11682732638fde5
Adam Celarek <git_commits@xibo.at>
2020-03-10 18:20:09 +0100

    update scripts (was error with naming, + new run)

--- prototype/experiment_dl_2.py
+++ prototype/experiment_dl_2.py
@@ -12,7 +12,7 @@ def generate_fitting_module(n_input_gaussians: int, n_output_gaussians: int) -> 
                                                n_dims=n_dimensions,
                                                aggregations=1, batch_norm=True)
 
-experiment_gm_mnist.experiment_alternating(device='cuda:1', n_epochs=100, desc_string="64-128-10n-20n-20n-25n-a1-128-128-128-64-64-64",
+experiment_gm_mnist.experiment_alternating(device='cuda:0', n_epochs=100, desc_string="64-128-10n-20n-20n-25n-a1-128-128-128-64-64-64",
                                            layer1_m2m_fitting=generate_fitting_module,
                                            layer2_m2m_fitting=generate_fitting_module,
                                            layer3_m2m_fitting=generate_fitting_module)
