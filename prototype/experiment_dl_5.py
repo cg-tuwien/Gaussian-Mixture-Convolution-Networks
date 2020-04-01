3863b9d3c776a29aa89b60728f1d66c37db3e2c7
Adam Celarek <git_commits@xibo.at>
2020-04-01 19:54:32 +0200

    remove experiment start files.

--- prototype/experiment_dl_5.py
+++
@@ -1,9 +0,0 @@
-from experiment_gm_fitting import test_dl_fitting
-import gm_fitting
-import experiment_gm_mnist
-
-experiment_gm_mnist.experiment_alternating(device='cuda:0', n_epochs=30, desc_string="learn_all",
-                                           learn_covariances=False, learn_positions=False)
-
-experiment_gm_mnist.experiment_alternating(device='cuda:0', n_epochs=70, desc_string="learn_all",
-                                           learn_covariances=True, learn_positions=True)
