6c43a27e5e32d057cc78bc9c5f1218ec0c299406
Adam Celarek <git_commits@xibo.at>
2020-04-02 23:21:47 +0200

    we did a nice refactor on the experiment start scripts. save the new config + usage example. ignore the rest.

---
+++ prototype/experiment_dl_conf.py
@@ -0,0 +1,37 @@
+import gm_fitting
+import experiment_gm_mnist
+import sys
+
+learning_rate_kernels = 0.0005
+learning_rate_fitting = 0.001
+
+
+def generate_fitting_module_XS(n_input_gaussians: int, n_output_gaussians: int) -> gm_fitting.Net:
+    assert n_output_gaussians > 0
+    n_dimensions = 2
+    return gm_fitting.PointNetWithParallelMLPs([64, 128, n_output_gaussians * 25],
+                                               [128, 64],
+                                               n_output_gaussians=n_output_gaussians,
+                                               n_dims=n_dimensions,
+                                               aggregations=1, batch_norm=True)
+
+
+def generate_fitting_module_S(n_input_gaussians: int, n_output_gaussians: int) -> gm_fitting.Net:
+    assert n_output_gaussians > 0
+    n_dimensions = 2
+    return gm_fitting.PointNetWithParallelMLPs([64, 128, 256, n_output_gaussians * 25],
+                                               [256, 128, 64],
+                                               n_output_gaussians=n_output_gaussians,
+                                               n_dims=n_dimensions,
+                                               aggregations=1, batch_norm=True)
+
+
+def generate_subdivide_fitting_module(n_gaussians_per_fitting_module, fitting_module):
+    def generate_fitting_module(n_input_gaussians: int, n_output_gaussians: int) -> gm_fitting.Net:
+        assert n_output_gaussians > 0
+        return gm_fitting.SpaceSubdivider(fitting_module,
+                                          n_input_gaussians=n_input_gaussians,
+                                          n_fitting_module_out_gaussians=n_gaussians_per_fitting_module,
+                                          n_output_gaussians=n_output_gaussians)
+
+    return generate_fitting_module
