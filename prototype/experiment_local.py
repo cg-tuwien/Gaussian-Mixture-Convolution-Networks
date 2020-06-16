1a06ecfc22e368dac19929aac5fba3dda928128f
Adam Celarek <git_commits@xibo.at>
2020-06-16 16:50:57 +0200

    better testing

--- prototype/experiment_local.py
+++ prototype/experiment_local.py
@@ -19,60 +19,129 @@ import gm_evaluate.gm_evaluate_inversed
 import gm
 import torch.autograd
 
-mixture = gm.generate_random_mixtures(3, 2, 60, 2)
-mixture = gm.pack_mixture(gm.weights(mixture), gm.positions(mixture), gm.covariances(mixture).inverse().transpose(-2, -1)).cuda()
-xes = torch.rand([3, 2, 60, 2]).cuda()
+enable_python = True
 
-# ref = gm.evaluate_inversed(mixture, xes)
-# out = gm_evaluate.gm_evaluate_inversed.apply(mixture, xes)
+mixture = gm.generate_random_mixtures(1, 1, 32000, 2)
+mixture = gm.pack_mixture(gm.weights(mixture), gm.positions(mixture), gm.covariances(mixture).inverse().transpose(-2, -1))
+xes = torch.rand([1, 1, 1000, 2])
 
-start_time = time.perf_counter()
-print("python started")
-ref = gm.evaluate_inversed(mixture, xes)
-ref_time = time.perf_counter()
-print("cpp started")
-out = gm_evaluate.gm_evaluate_inversed.apply(mixture, xes)
-cpp_time = time.perf_counter()
+cuda_mixture = mixture.cuda()
+cuda_xes = xes.cuda()
+
+print("warming up")
+if enable_python:
+    python_cpu = gm.evaluate_inversed(mixture, xes)
+    python_cuda = gm.evaluate_inversed(cuda_mixture, cuda_xes)
+cpp_cpu = gm_evaluate.gm_evaluate_inversed.apply(mixture, xes)
+cpp_cuda = gm_evaluate.gm_evaluate_inversed.apply(cuda_mixture, cuda_xes)
+
+if enable_python:
+    print("python cpu started")
+    python_cpu_start_time = time.perf_counter()
+    python_cpu = gm.evaluate_inversed(mixture, xes)
+    python_cpu_end_time = time.perf_counter()
+
+    print("python cuda started")
+    python_cuda_start_time = time.perf_counter()
+    python_cuda = gm.evaluate_inversed(cuda_mixture, cuda_xes)
+    python_cuda_end_time = time.perf_counter()
+
+print("cpp cpu started")
+cpp_cpu_start_time = time.perf_counter()
+cpp_cpu = gm_evaluate.gm_evaluate_inversed.apply(mixture, xes)
+cpp_cpu_end_time = time.perf_counter()
+
+print("cpp cuda started")
+cpp_cuda_start_time = time.perf_counter()
+cpp_cuda = gm_evaluate.gm_evaluate_inversed.apply(cuda_mixture, cuda_xes)
+cpp_cuda_end_time = time.perf_counter()
 
 print(f"====== requires_grad = False ======")
-print(f"RMSE: {((ref - out)**2).mean().sqrt().item()}")
-print(f"python: {ref_time - start_time}")
-print(f"cpp: {cpp_time - ref_time}")
+if enable_python:
+    print(f"RMSE python_cpu vs python_cuda: {((python_cpu - python_cuda.cpu())**2).mean().sqrt().item()}")
+    print(f"RMSE python_cpu vs cpp_cpu: {((python_cpu - cpp_cpu)**2).mean().sqrt().item()}")
+    print(f"RMSE python_cpu vs cpp_cuda: {((python_cpu - cpp_cuda.cpu())**2).mean().sqrt().item()}")
+print(f"RMSE cpp_cpu vs cpp_cuda: {((cpp_cpu - cpp_cuda.cpu())**2).mean().sqrt().item()}")
+if enable_python:
+    print(f"python cpu: {python_cpu_end_time - python_cpu_start_time}")
+    print(f"python cuda: {python_cuda_end_time - python_cuda_start_time}")
+print(f"cpp cpu: {cpp_cpu_end_time - cpp_cpu_start_time}")
+print(f"cpp cuda: {cpp_cuda_end_time - cpp_cuda_start_time}")
 
 print(f"====== requires_grad = True ======")
 mixture.requires_grad = True;
 xes.requires_grad = True;
+cuda_mixture.requires_grad = True;
+cuda_xes.requires_grad = True;
 
-print("cpu forward started")
-cpu_start = time.perf_counter()
-out = gm_evaluate.gm_evaluate_inversed.apply(mixture, xes)
-cpu_forward = time.perf_counter()
-print("cpu backward started")
-out.sum().backward()
-cpu_backward = time.perf_counter()
-cpu_mixture_grad = mixture.grad.clone()
-cpu_xes_grad = xes.grad.clone()
 
-xes.grad = None
+if enable_python:
+    print("python cpu started")
+    python_cpu_start_time = time.perf_counter()
+    python_cpu = gm.evaluate_inversed(mixture, xes)
+    python_cpu_forward_time = time.perf_counter()
+    python_cpu.sum().backward()
+    python_cpu_end_time = time.perf_counter()
+    python_cpu_mixture_grad = mixture.grad.clone()
+    python_cpu_xes_grad = xes.grad.clone()
+
+    print("python cuda started")
+    python_cuda_start_time = time.perf_counter()
+    python_cuda = gm.evaluate_inversed(cuda_mixture, cuda_xes)
+    python_cuda_forward_time = time.perf_counter()
+    python_cuda.sum().backward()
+    python_cuda_end_time = time.perf_counter()
+    python_cuda_mixture_grad = cuda_mixture.grad.clone()
+    python_cuda_xes_grad = cuda_xes.grad.clone()
+
 mixture.grad = None
+xes.grad = None
+cuda_mixture.grad = None
+cuda_xes.grad = None
 
-print("python forward started")
-python_start = time.perf_counter()
-ref = gm.evaluate_inversed(mixture, xes)
-python_forward = time.perf_counter()
-print("python backward started")
-ref.sum().backward()
-python_backward = time.perf_counter()
-pyhton_mixture_grad = mixture.grad.clone()
-pyhton_xes_grad = xes.grad.clone()
+print("cpp cpu started")
+cpp_cpu_start_time = time.perf_counter()
+cpp_cpu = gm_evaluate.gm_evaluate_inversed.apply(mixture, xes)
+cpp_cpu_forward_time = time.perf_counter()
+cpp_cpu.sum().backward()
+cpp_cpu_end_time = time.perf_counter()
+cpp_cpu_mixture_grad = mixture.grad.clone()
+cpp_cpu_xes_grad = xes.grad.clone()
 
 
-print(f"mixture grad RMSE: {((pyhton_mixture_grad - cpu_mixture_grad)**2).mean().sqrt().item()}")
-print(f"xes grad RMSE: {((pyhton_xes_grad - cpu_xes_grad)**2).mean().sqrt().item()}")
-print(f"python forward: {python_forward - python_start}")
-print(f"python backward: {python_backward - python_forward}")
-print(f"cpu forward: {cpu_forward - cpu_start}")
-print(f"cpu backward: {cpu_backward - cpu_forward}")
+print("cpp cuda started")
+cpp_cuda_start_time = time.perf_counter()
+cpp_cuda = gm_evaluate.gm_evaluate_inversed.apply(cuda_mixture, cuda_xes)
+cpp_cuda_forward_time = time.perf_counter()
+cpp_cuda.sum().backward()
+cpp_cuda_end_time = time.perf_counter()
+cpp_cuda_mixture_grad = cuda_mixture.grad.clone()
+cpp_cuda_xes_grad = cuda_xes.grad.clone()
+
+
+
+if enable_python:
+    print(f"RMSE mixture grad python_cpu vs python_cuda: {((python_cpu_mixture_grad - python_cuda_mixture_grad.cpu())**2).mean().sqrt().item()}")
+    print(f"RMSE  mixture grad python_cpu vs cpp_cpu: {((python_cpu_mixture_grad - cpp_cpu_mixture_grad)**2).mean().sqrt().item()}")
+    print(f"RMSE mixture grad python_cpu vs cpp_cuda: {((python_cpu_mixture_grad - cpp_cuda_mixture_grad.cpu())**2).mean().sqrt().item()}")
+print(f"RMSE mixture grad cpp_cpu vs cpp_cuda: {((cpp_cpu_mixture_grad - cpp_cuda_mixture_grad.cpu())**2).mean().sqrt().item()}")
+
+if enable_python:
+    print(f"RMSE xes grad python_cpu vs python_cuda: {((python_cpu_xes_grad - python_cuda_xes_grad.cpu())**2).mean().sqrt().item()}")
+    print(f"RMSE  xes grad python_cpu vs cpp_cpu: {((python_cpu_xes_grad - cpp_cpu_xes_grad)**2).mean().sqrt().item()}")
+    print(f"RMSE xes grad python_cpu vs cpp_cuda: {((python_cpu_xes_grad - cpp_cuda_xes_grad.cpu())**2).mean().sqrt().item()}")
+print(f"RMSE xes grad cpp_cpu vs cpp_cuda: {((cpp_cpu_xes_grad - cpp_cuda_xes_grad.cpu())**2).mean().sqrt().item()}")
+
+if enable_python:
+    print(f"python cpu forward: {python_cpu_forward_time - python_cpu_start_time}")
+    print(f"python cpu backward: {python_cpu_end_time - python_cpu_forward_time}")
+    print(f"python cuda forward: {python_cuda_forward_time - python_cuda_start_time}")
+    print(f"python cuda backward: {python_cuda_end_time - python_cuda_forward_time}")
+print(f"cpp cpu forward: {cpp_cpu_forward_time - cpp_cpu_start_time}")
+print(f"cpp cpu backward: {cpp_cpu_end_time - cpp_cpu_forward_time}")
+print(f"cpp cuda forward: {cpp_cuda_forward_time - cpp_cuda_start_time}")
+print(f"cpp cuda backward: {cpp_cuda_end_time - cpp_cuda_forward_time}")
+
 
 # gradcheck takes a tuple of tensors as input, check if your gradient
 # evaluated with these tensors are close enough to numerical
