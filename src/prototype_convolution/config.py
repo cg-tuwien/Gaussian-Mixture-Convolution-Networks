1535b74957a2331aff9f6584ef1e0bebecb0d8ad
Adam Celarek <git_commits@xibo.at>
2020-07-31 15:02:22 +0200

    python execution fix on the cluster

--- src/prototype_convolution/config.py
+++ src/prototype_convolution/config.py
@@ -6,6 +6,13 @@ data_base_path = pathlib.Path(f"{source_dir}/../../data")
 num_dataloader_workers = 0
 batch_size = 100
 
+# mnist_n_in_g = 25
+# mnist_n_layers_1 = 10
+# mnist_n_out_g_1 = 24
+# mnist_n_layers_2 = 12
+# mnist_n_out_g_2 = 24
+# mnist_n_out_g_3 = 12
+
 mnist_n_in_g = 25
 mnist_n_layers_1 = 5
 mnist_n_out_g_1 = 24
