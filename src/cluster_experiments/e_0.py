e48bdc0ceeaaa998ad458700d096402215cb7880
Adam Celarek <git_commits@xibo.at>
2020-07-31 17:26:48 +0200

    update cluster experiment config

--- src/cluster_experiments/e_0.py
+++ src/cluster_experiments/e_0.py
@@ -6,4 +6,4 @@ device = list(sys.argv)[1]
 #device = "cuda"
 
 experiment_gm_mnist.experiment(device=device, n_epochs=200, desc_string="nobias_oaBN_k001", kernel_learning_rate=0.001, learn_covariances_after=200,
-                               learn_positions_after=200, log_interval=1000, use_bias=False, batch_norm_per_layer=False, use_adam=False, gmcn_config=gmcn_config)
+                               learn_positions_after=200, log_interval=1000, use_bias=False, batch_norm_per_layer=False, use_adam=True, gmcn_config=gmcn_config)
