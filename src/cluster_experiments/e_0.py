cdb8aef9eff8b2d1fc3011bdfa5f3663282b3b9d
Adam Celarek <git_commits@xibo.at>
2020-08-13 18:18:03 +0200

    new experiment config

--- src/cluster_experiments/e_0.py
+++ src/cluster_experiments/e_0.py
@@ -5,5 +5,40 @@ import prototype_convolution.config as gmcn_config
 device = list(sys.argv)[1]
 #device = "cuda"
 
-experiment_gm_mnist.experiment(device=device, n_epochs=200, desc_string="k001", kernel_learning_rate=0.001, learn_covariances_after=200,
-                               learn_positions_after=200, log_interval=1000, use_bias=True, batch_norm_per_layer=True, use_adam=True, gmcn_config=gmcn_config)
+gmcn_config.mnist_n_in_g = 25
+gmcn_config.mnist_n_layers_1 = 8
+gmcn_config.mnist_n_out_g_1 = 20
+gmcn_config.mnist_n_layers_2 = 10
+gmcn_config.mnist_n_out_g_2 = 8
+gmcn_config.mnist_n_out_g_3 = 8
+
+gmcn_config.bias_type = gmcn_config.BIAS_TYPE_NONE
+gmcn_config.bn_constant_computation = gmcn_config.BN_CONSTANT_COMPUTATION_ZERO
+gmcn_config.bn_mean_over_layers = False
+
+experiment_gm_mnist.experiment(device=device, n_epochs=100, desc_string="M3_biasNone_bnCCzero", kernel_learning_rate=0.001, learn_covariances_after=200,
+                               learn_positions_after=200, log_interval=1000, gmcn_config=gmcn_config)
+
+gmcn_config.bias_type = gmcn_config.BIAS_TYPE_NONE
+gmcn_config.bn_constant_computation = gmcn_config.BN_CONSTANT_COMPUTATION_ZERO
+gmcn_config.bn_mean_over_layers = True
+
+experiment_gm_mnist.experiment(device=device, n_epochs=100, desc_string="M3_biasNone_bnCCzero_bnLayerMean", kernel_learning_rate=0.001, learn_covariances_after=200,
+                               learn_positions_after=200, log_interval=1000, gmcn_config=gmcn_config)
+
+
+gmcn_config.bias_type = gmcn_config.BIAS_TYPE_NONE
+gmcn_config.bn_constant_computation = gmcn_config.BN_CONSTANT_COMPUTATION_ZERO
+gmcn_config.bn_mean_over_layers = False
+
+experiment_gm_mnist.experiment(device=device, n_epochs=100, desc_string="M3_biasNone_bnCCzero_learnAll50", kernel_learning_rate=0.001, learn_covariances_after=50,
+                               learn_positions_after=50, log_interval=1000, gmcn_config=gmcn_config)
+
+gmcn_config.bias_type = gmcn_config.BIAS_TYPE_NONE
+gmcn_config.bn_constant_computation = gmcn_config.BN_CONSTANT_COMPUTATION_ZERO
+gmcn_config.bn_mean_over_layers = True
+
+experiment_gm_mnist.experiment(device=device, n_epochs=100, desc_string="M3_biasNone_bnCCzero_bnLayerMean_learnAll50", kernel_learning_rate=0.001, learn_covariances_after=50,
+                               learn_positions_after=50, log_interval=1000, gmcn_config=gmcn_config)
+
+
--- src/cluster_experiments/e_1.py
+++ src/cluster_experiments/e_1.py
@@ -6,11 +6,38 @@ device = list(sys.argv)[1]
 #device = "cuda"
 
 gmcn_config.mnist_n_in_g = 25
-gmcn_config.mnist_n_layers_1 = 10
-gmcn_config.mnist_n_out_g_1 = 24
-gmcn_config.mnist_n_layers_2 = 8
-gmcn_config.mnist_n_out_g_2 = 10
+gmcn_config.mnist_n_layers_1 = 8
+gmcn_config.mnist_n_out_g_1 = 20
+gmcn_config.mnist_n_layers_2 = 10
+gmcn_config.mnist_n_out_g_2 = 8
 gmcn_config.mnist_n_out_g_3 = 8
 
-experiment_gm_mnist.experiment(device=device, n_epochs=200, desc_string="M2_k001", kernel_learning_rate=0.001, learn_covariances_after=200,
-                               learn_positions_after=200, log_interval=1000, use_bias=True, batch_norm_per_layer=True, use_adam=True, gmcn_config=gmcn_config)
+gmcn_config.bias_type = gmcn_config.BIAS_TYPE_NORMAL
+gmcn_config.bn_constant_computation = gmcn_config.BN_CONSTANT_COMPUTATION_ZERO
+gmcn_config.bn_mean_over_layers = False
+
+experiment_gm_mnist.experiment(device=device, n_epochs=100, desc_string="M3_biasYes_bnCCzero", kernel_learning_rate=0.001, learn_covariances_after=200,
+                               learn_positions_after=200, log_interval=1000, gmcn_config=gmcn_config)
+
+gmcn_config.bias_type = gmcn_config.BIAS_TYPE_NORMAL
+gmcn_config.bn_constant_computation = gmcn_config.BN_CONSTANT_COMPUTATION_ZERO
+gmcn_config.bn_mean_over_layers = True
+
+experiment_gm_mnist.experiment(device=device, n_epochs=100, desc_string="M3_biasYes_bnCCzero_bnLayerMean", kernel_learning_rate=0.001, learn_covariances_after=200,
+                               learn_positions_after=200, log_interval=1000, gmcn_config=gmcn_config)
+
+
+gmcn_config.bias_type = gmcn_config.BIAS_TYPE_NEGATIVE_SOFTPLUS
+gmcn_config.bn_constant_computation = gmcn_config.BN_CONSTANT_COMPUTATION_ZERO
+gmcn_config.bn_mean_over_layers = False
+
+experiment_gm_mnist.experiment(device=device, n_epochs=100, desc_string="M3_biasNSP_bnCCzero", kernel_learning_rate=0.001, learn_covariances_after=100,
+                               learn_positions_after=100, log_interval=1000, gmcn_config=gmcn_config)
+
+gmcn_config.bias_type = gmcn_config.BIAS_TYPE_NEGATIVE_SOFTPLUS
+gmcn_config.bn_constant_computation = gmcn_config.BN_CONSTANT_COMPUTATION_ZERO
+gmcn_config.bn_mean_over_layers = True
+
+experiment_gm_mnist.experiment(device=device, n_epochs=100, desc_string="M3_biasNSP_bnCCzero_bnLayerMean", kernel_learning_rate=0.001, learn_covariances_after=100,
+                               learn_positions_after=100, log_interval=1000, gmcn_config=gmcn_config)
+
