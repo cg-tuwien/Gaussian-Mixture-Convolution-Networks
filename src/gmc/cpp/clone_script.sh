bd2f70aba8f32552b0c90cb41fd927883d779efc
Adam Ce <adam@dlmain.cluster>
2020-08-24 16:26:15 +0200

    correct clone script

--- src/gmc/cpp/clone_script.sh
+++ src/gmc/cpp/clone_script.sh
@@ -1,2 +1,2 @@
-git clone git@github.com:SFraissTU/da-gm-1.git
-git clone https://github.com/g-truc/glm.git gm_vis
+git clone git@github.com:SFraissTU/da-gm-1.git gm_vis
+git clone https://github.com/g-truc/glm.git
