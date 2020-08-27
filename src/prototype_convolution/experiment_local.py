ac34ab9a0d0d33bccb332f3f753202cb0d86169a
Adam Celarek <git_commits@xibo.at>
2020-08-27 11:15:35 +0200

    refactor experiment runners

--- src/prototype_convolution/experiment_local.py
+++ src/prototype_convolution/experiment_local.py
@@ -1,10 +1,14 @@
 import sys
-import prototype_convolution.config as gmcn_config
 import cluster_experiments.conf as cluster
+import prototype_convolution.config
+import prototype_convolution.fitting
 
 # device = list(sys.argv)[1]
 device = "cuda"
 
+gmcn_config: prototype_convolution.config = cluster.default_gmcn_config()
 gmcn_config.bias_type = gmcn_config.BIAS_TYPE_NONE
-gmcn_config.bn_place = gmcn_config.BN_PLACE_AFTER_GMC
-cluster.run_with(device, "biasNone_bnAfterGmc", gmcn_config)
+gmcn_config.bn_place = gmcn_config.BN_PLACE_BEFORE_GMC
+gmcn_config.fitting_config.representative_select_mode = prototype_convolution.fitting.Config.REPRESENTATIVE_SELECT_MODE_RANDOM_TOP
+
+cluster.run_with(device, "fitRndTop_biasNone_bnAfterGmc", gmcn_config)
