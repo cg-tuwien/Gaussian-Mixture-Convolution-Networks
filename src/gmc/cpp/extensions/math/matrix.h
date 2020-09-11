160ad88c16846a1a3b47e17656f70259696668c0
Adam Celarek <git_commits@xibo.at>
2020-09-11 12:53:54 +0200

    replace type traits based const handling with easier to read overloads

--- src/gmc/cpp/extensions/math/matrix.h
+++ src/gmc/cpp/extensions/math/matrix.h
@@ -27,15 +27,25 @@ __forceinline__ __host__ __device__ scalar_t trace(const glm::mat<3, 3, scalar_t
 }
 
 template <int DIMS, typename scalar_t>
-__forceinline__ __host__ __device__ typename std::conditional<std::is_const<scalar_t>::value, const glm::vec<DIMS, std::remove_cv_t<scalar_t>>, glm::vec<DIMS, scalar_t>>::type&
+__forceinline__ __host__ __device__ glm::vec<DIMS, scalar_t>&
 vec(scalar_t& memory_location) {
-    return reinterpret_cast<typename std::conditional<std::is_const<scalar_t>::value, const glm::vec<DIMS, std::remove_cv_t<scalar_t>>, glm::vec<DIMS, scalar_t>>::type&>(memory_location);
+    return reinterpret_cast<glm::vec<DIMS, scalar_t>&>(memory_location);
 }
 
 template <int DIMS, typename scalar_t>
-__forceinline__ __host__ __device__ typename std::conditional<std::is_const<scalar_t>::value, const glm::mat<DIMS, DIMS, std::remove_cv_t<scalar_t>>, glm::mat<DIMS, DIMS, scalar_t>>::type&
-mat(scalar_t& memory_location) {
-    return reinterpret_cast<typename std::conditional<std::is_const<scalar_t>::value, const glm::mat<DIMS, DIMS, std::remove_cv_t<scalar_t>>, glm::mat<DIMS, DIMS, scalar_t>>::type&>(memory_location);
+__forceinline__ __host__ __device__ const glm::vec<DIMS, scalar_t>&
+vec(const scalar_t& memory_location) {
+    return reinterpret_cast<const glm::vec<DIMS, scalar_t>&>(memory_location);
+}
+
+template <int DIMS, typename scalar_t>
+__forceinline__ __host__ __device__ const glm::mat<DIMS, DIMS, scalar_t>& mat(const scalar_t& memory_location) {
+    return reinterpret_cast<const glm::mat<DIMS, DIMS, scalar_t>&>(memory_location);
+}
+
+template <int DIMS, typename scalar_t>
+__forceinline__ __host__ __device__ glm::mat<DIMS, DIMS, scalar_t>& mat(scalar_t& memory_location) {
+    return reinterpret_cast<glm::mat<DIMS, DIMS, scalar_t>&>(memory_location);
 }
 
 }
