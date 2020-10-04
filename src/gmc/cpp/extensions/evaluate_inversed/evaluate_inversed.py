a186f085e2ee2abc9a8944cdcfbbc935fb159b9e
SFraissTU <e1425602@student.tuwien.ac.at>
2020-10-04 20:17:13 +0200

    Fixes for windows

--- src/gmc/cpp/extensions/evaluate_inversed/evaluate_inversed.py
+++ src/gmc/cpp/extensions/evaluate_inversed/evaluate_inversed.py
@@ -12,10 +12,10 @@ extra_include_paths = [source_dir + "/../../glm/", source_dir + "/../../cub/", s
 #                                 extra_include_paths=extra_include_paths,
 #                                 verbose=True, extra_cflags=cuda_extra_cflags, extra_cuda_cflags=cuda_extra_cuda_cflags, extra_ldflags=["-lpthread"])
 
-cuda_bvh = load('evaluate_inversed_cuda_bvh', [source_dir + '/cuda_bvh.cpp', source_dir + '/cuda_bvh.cu', source_dir + '/../lbvh/bvh.cu',
-                                               source_dir + '/../math/symeig_cuda.cpp', source_dir + '/../math/symeig_cuda.cu'],
-                                extra_include_paths=extra_include_paths,
-                                verbose=True, extra_cflags=cuda_extra_cflags, extra_cuda_cflags=cuda_extra_cuda_cflags, extra_ldflags=["-lpthread"])
+#cuda_bvh = load('evaluate_inversed_cuda_bvh', [source_dir + '/cuda_bvh.cpp', source_dir + '/cuda_bvh.cu', source_dir + '/../lbvh/bvh.cu',
+#                                               source_dir + '/../math/symeig_cuda.cpp', source_dir + '/../math/symeig_cuda.cu'],
+#                                extra_include_paths=extra_include_paths,
+#                                verbose=True, extra_cflags=cuda_extra_cflags, extra_cuda_cflags=cuda_extra_cuda_cflags, extra_ldflags=["-lpthread"])
 
 parallel = load('evaluate_inversed_parallel',
                 [source_dir + '/parallel_binding.cpp', source_dir + '/parallel_implementation.cu'],
