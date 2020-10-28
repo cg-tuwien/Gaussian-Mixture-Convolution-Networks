ef796c1b0d7fe2f4dc9edf436d4c8cfce00a4bba
Adam Celarek <git_commits@xibo.at>
2020-10-28 17:35:10 +0100

    c++: more container algorithms with unit tests

--- src/gmc/cpp/extensions/containers.h
+++ src/gmc/cpp/extensions/containers.h
@@ -95,6 +95,15 @@ struct Vector {
         m_size = size;
     }
 
+    static Vector<T, N, size_type> filled(const T& value, unsigned n_elements) {
+        assert(n_elements < N);
+        Vector<T, N, size_type> vec;
+        vec.resize(n_elements);
+        for (int i = 0; i < n_elements; ++i) {
+            vec[i] = value;
+        }
+    }
+
     __host__ __device__ __forceinline__
     T& operator[](size_t i) {
         assert(i < m_size);
@@ -175,8 +184,20 @@ T plus(const T& a, const T& b) {
     return a + b;
 }
 template<typename T>
+T minus(const T& a, const T& b) {
+    return a - b;
+}
+template<typename T>
 T times(const T& a, const T& b) {
     return a * b;
+}
+template<typename T>
+T divided_AbyB(const T& a, const T& b) {
+    return a / b;
+}
+template<typename T>
+T divided_BbyA(const T& a, const T& b) {
+    return b / a;
 }
 template<typename T>
 bool logical_and(const T& a, const T& b) {
@@ -217,6 +238,35 @@ auto outer_product(const gpe::Vector<T1, N1>& m1,
     return matrix;
 }
 
+template<typename T, uint32_t N, typename Function>
+__host__ __device__ __forceinline__
+auto transform(const gpe::Vector<T, N>& vec, Function fun) -> Vector<decltype (fun(vec.front())), N> {
+    using ProductType = decltype (fun(vec.front()));
+    gpe::Vector<ProductType, N> retvec;
+    retvec.resize(vec.size());
+    for (unsigned i = 0; i < vec.size(); ++i) {
+        retvec[i] = fun(vec[i]);
+    }
+    return retvec;
+}
+
+template<typename T, uint32_t N1, uint32_t N2, typename Function>
+__host__ __device__ __forceinline__
+auto transform(const gpe::Vector2d<T, N1, N2>& mat, Function fun) -> Vector2d<decltype (fun(mat.front().front())), N1, N2> {
+    using ProductType = decltype (fun(mat.front().front()));
+    gpe::Vector2d<ProductType, N1, N2> retmat;
+    retmat.resize(mat.size());
+    for (unsigned i = 0; i < mat.size(); ++i) {
+        assert(mat[0].size() == mat[i].size());
+        assert(mat[i].size() == mat[i].size());
+        retmat[i].resize(mat[i].size());
+        for (unsigned j = 0; j < mat.size(); ++j) {
+            retmat[i][j] = fun(mat[i][j]);
+        }
+    }
+    return retmat;
+}
+
 template<typename T1, typename T2, uint32_t N, typename Function>
 __host__ __device__ __forceinline__
 auto cwise_fun(const gpe::Vector<T1, N>& m1,
@@ -244,10 +294,58 @@ auto cwise_fun(const Vector2d<T1, N1, N2>& m1,
     gpe::Vector2d<ProductType, N1, N2> matrix;
     matrix.resize(m1.size());
     for (unsigned i = 0; i < m1.size(); ++i) {
-        matrix[i].resize(m2.size());
-        for (unsigned j = 0; j < m2.size(); ++j) {
+        assert(m1[0].size() == m1[i].size());
+        assert(m1[i].size() == m2[i].size());
+        matrix[i].resize(m1[i].size());
+        for (unsigned j = 0; j < m1[i].size(); ++j) {
             const T1& a = m1[i][j];
             const T2& b = m2[i][j];
+            ProductType v = fun(a, b);
+            matrix[i][j] = v;
+        }
+    }
+    return matrix;
+}
+
+/// multiplies every row in m with the corresponding element in v (column vector)
+template<typename T1, typename T2, uint32_t N1, uint32_t N2, typename Function>
+__host__ __device__ __forceinline__
+auto cwise_fun(const Vector2d<T1, N1, N2>& m,
+               const Vector<T2, N1>& v,
+               Function fun) -> Vector2d<decltype (fun(m.front().front(), v.front())), N1, N2> {
+    using ProductType = decltype (fun(m.front().front(), v.front()));
+    gpe::Vector2d<ProductType, N1, N2> matrix;
+    assert(m.size() == v.size());
+    matrix.resize(m.size());
+    for (unsigned i = 0; i < m.size(); ++i) {
+        assert(m[0].size() == m[i].size());
+        matrix[i].resize(m[i].size());
+        for (unsigned j = 0; j < m[i].size(); ++j) {
+            const T1& a = m[i][j];
+            const T2& b = v[i];
+            ProductType v = fun(a, b);
+            matrix[i][j] = v;
+        }
+    }
+    return matrix;
+}
+
+/// multiplies every column in m with the corresponding element in v (row vector)
+template<typename T1, typename T2, uint32_t N1, uint32_t N2, typename Function>
+__host__ __device__ __forceinline__
+auto cwise_fun(const Vector<T1, N2>& v,
+               const Vector2d<T2, N1, N2>& m,
+               Function fun) -> Vector2d<decltype (fun(v.front(), m.front().front())), N1, N2> {
+    using ProductType = decltype (fun(v.front(), m.front().front()));
+    gpe::Vector2d<ProductType, N1, N2> matrix;
+    matrix.resize(m.size());
+    for (unsigned i = 0; i < m.size(); ++i) {
+        assert(m[0].size() == v.size());
+        assert(m[0].size() == m[i].size());
+        matrix[i].resize(m[i].size());
+        for (unsigned j = 0; j < m[i].size(); ++j) {
+            const T2& a = m[i][j];
+            const T1& b = v[j];
             ProductType v = fun(a, b);
             matrix[i][j] = v;
         }
@@ -271,6 +369,39 @@ T2 reduce(const gpe::Vector2d<T1, N1, N2>& matrix, T2 initial, Function fun) {
     for (unsigned i = 0; i < matrix.size(); ++i) {
         for (unsigned j = 0; j < matrix[i].size(); ++j) {
             initial = fun(initial, matrix[i][j]);
+        }
+    }
+    return initial;
+}
+
+template<typename T1, typename T2, uint32_t N1, uint32_t N2, typename Function>
+__host__ __device__ __forceinline__
+gpe::Vector<T2, N1> reduce_rows(const gpe::Vector2d<T1, N1, N2>& matrix, T2 initial, Function fun) {
+    gpe::Vector<T2, N1> retvec;
+    retvec.resize(matrix.size());
+    for (unsigned i = 0; i < matrix.size(); ++i) {
+        retvec[i] = initial;
+        for (unsigned j = 0; j < matrix[i].size(); ++j) {
+            retvec[i] = fun(retvec[i], matrix[i][j]);
+        }
+    }
+    return initial;
+}
+
+template<typename T1, typename T2, uint32_t N1, uint32_t N2, typename Function>
+__host__ __device__ __forceinline__
+    gpe::Vector<T2, N2> reduce_cols(const gpe::Vector2d<T1, N1, N2>& matrix, T2 initial, Function fun) {
+    gpe::Vector<T2, N2> retvec;
+    if (matrix[0].size()) {
+        retvec.resize(matrix[0].size());
+        for (unsigned j = 0; j < retvec.size(); ++j) {
+            retvec[j] = initial;
+        }
+    }
+    for (unsigned i = 0; i < matrix.size(); ++i) {
+        assert(matrix[0].size() == matrix[i].size());
+        for (unsigned j = 0; j < matrix[i].size(); ++j) {
+            retvec[j] = fun(retvec[j], matrix[i][j]);
         }
     }
     return initial;
--- src/gmc/cpp/extensions/unit_tests.cpp
+++ src/gmc/cpp/extensions/unit_tests.cpp
@@ -6,13 +6,26 @@ namespace  {
 
 static struct UnitTests {
     UnitTests() {
+        testVector0Init();
         testBitSet();
         testOuterProduct();
+        testTransform1d();
+        testTransform2d();
         testCwiseFun1d();
         testCwiseFun2d();
+        testCwiseFun2dX1d();
+        testCwiseFun1dX2d();
         testReduce1d();
         testReduce1dBool();
         testReduce2d();
+        testReduceRows();
+        testReduceCols();
+    }
+
+    void testVector0Init() {
+        gpe::Vector<int, 1024> v = {0};
+        for (unsigned i = 0; i < 1024; i++)
+            assert(v[i] == 0);
     }
 
     void testBitSet() {
@@ -64,6 +77,23 @@ static struct UnitTests {
         assert(result[2][0] == 13); assert(result[2][1] == 23); assert(result[2][2] == 33); assert(result[2][3] == 43);
     }
 
+    void testTransform1d() {
+        Vector<int, 3> m1 = {1, 2, 3};
+        Vector<int, 3> result = transform(m1, [](int v) { return v * 10; });
+        assert(result[0] == 10);
+        assert(result[1] == 20);
+        assert(result[2] == 30);
+    }
+
+    void testTransform2d() {
+        Vector2d<int, 2, 3> m1;
+        m1.push_back(Vector<int, 3>{1, 2, 3});
+        m1.push_back(Vector<int, 3>{4, 5, 6});
+        Vector2d<int, 2, 3> result = transform(m1, [](int v) { return v + 10; });
+        assert(result[0][0] == 11); assert(result[0][1] == 12); assert(result[0][2] == 13);
+        assert(result[1][0] == 14); assert(result[1][1] == 15); assert(result[1][2] == 16);
+    }
+
     void testCwiseFun1d() {
         Vector<int, 3> m1 = {1, 2, 3};
         Vector<int, 3> m2 = {10, 20, 30};
@@ -83,6 +113,26 @@ static struct UnitTests {
         Vector2d<int, 2> result = cwise_fun(m1, m2, functors::times<int>);
         assert(result[0][0] == 10); assert(result[0][1] == 200);
         assert(result[1][0] == 3000); assert(result[1][1] == 40000);
+    }
+
+    void testCwiseFun2dX1d() {
+        Vector2d<int, 2, 3> m1;
+        m1.push_back(Vector<int, 3>{1, 2, 3});
+        m1.push_back(Vector<int, 3>{4, 5, 6});
+        Vector<int, 2> v = {1, 2};
+        Vector2d<int, 2, 3> result = cwise_fun(m1, v, functors::times<int>);
+        assert(result[0][0] == 1); assert(result[0][1] == 2); assert(result[0][2] == 3);
+        assert(result[1][0] == 8); assert(result[1][1] == 10); assert(result[1][2] == 12);
+    }
+
+    void testCwiseFun1dX2d() {
+        Vector<int, 3> v = {1, 2, 10};
+        Vector2d<int, 2, 3> m1;
+        m1.push_back(Vector<int, 3>{1, 2, 3});
+        m1.push_back(Vector<int, 3>{4, 5, 6});
+        Vector2d<int, 2, 3> result = cwise_fun(v, m1, functors::times<int>);
+        assert(result[0][0] == 1); assert(result[0][1] == 4); assert(result[0][2] == 30);
+        assert(result[1][0] == 4); assert(result[1][1] == 10); assert(result[1][2] == 60);
     }
 
     void testReduce1d() {
@@ -110,6 +160,24 @@ static struct UnitTests {
         m1.push_back(Vector<int, 2>{3, 4});
         int result = reduce(m1, int(1), functors::times<int>);
         assert(result == 1*2*3*4);
+    }
+
+    void testReduceRows() {
+        Vector2d<int, 2> m1;
+        m1.push_back(Vector<int, 2>{1, 2});
+        m1.push_back(Vector<int, 2>{3, 4});
+        auto result = reduce_rows(m1, int(1), functors::times<int>);
+        assert(result[0] == 1*2);
+        assert(result[1] == 3*4);
+    }
+
+    void testReduceCols() {
+        Vector2d<int, 2> m1;
+        m1.push_back(Vector<int, 2>{1, 2});
+        m1.push_back(Vector<int, 2>{3, 4});
+        auto result = reduce_cols(m1, int(1), functors::times<int>);
+        assert(result[0] == 1*3);
+        assert(result[1] == 2*4);
     }
 } unit_tests;
 
