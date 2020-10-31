970790131b5829abc7cb3747875a4bab3707a03a
Adam Celarek <git_commits@xibo.at>
2020-10-31 19:28:55 +0100

    bvhfit / cpp: compute and print mse

--- src/gmc/cpp/extensions/bvh_mhem_fit/bvh_fit_tester.cpp
+++ src/gmc/cpp/extensions/bvh_mhem_fit/bvh_fit_tester.cpp
@@ -17,26 +17,26 @@
 #include "bvh_mhem_fit/bindings.h"
 
 constexpr uint N_BATCHES = 1;
-constexpr uint N_CONVOLUTION_LAYERS = 1;
+constexpr uint N_CONVOLUTION_LAYERS = 3;
 constexpr uint LIMIT_N_BATCH = 100;
 constexpr bool USE_CUDA = false;
 //constexpr bool BACKWARD = false;
 constexpr bool RENDER = true;
 
-void show(torch::Tensor mixture, const int resolution, const int n_batch_limit) {
+torch::Tensor render(torch::Tensor mixture, const int resolution, const int n_batch_limit) {
     using namespace torch::indexing;
     mixture = mixture.cuda();
     const auto n_batch = std::min(gpe::n_batch(mixture), n_batch_limit);
     mixture = mixture.index({Slice(None, n_batch)});
     const auto n_layers = gpe::n_layers(mixture);
 
-//    const auto weights = gpe::weights(mixture);
+    //    const auto weights = gpe::weights(mixture);
     const auto positions = gpe::positions(mixture);
-//    const auto invCovs = gpe::covariances(mixture).inverse().transpose(-1, -2);
-//    mixture = gpe::pack_mixture(weights, positions, invCovs.contiguous());
+    //    const auto invCovs = gpe::covariances(mixture).inverse().transpose(-1, -2);
+    //    mixture = gpe::pack_mixture(weights, positions, invCovs.contiguous());
 
-//    const auto minPos = positions.min().item().toFloat() - 1.1f;
-//    const auto maxPos = positions.max().item().toFloat() + 1.1f;
+    //    const auto minPos = positions.min().item().toFloat() - 1.1f;
+    //    const auto maxPos = positions.max().item().toFloat() + 1.1f;
 
     const auto minPos = -1.0f;
     const auto maxPos = 33.0f;
@@ -47,9 +47,13 @@ void show(torch::Tensor mixture, const int resolution, const int n_batch_limit) 
     auto yv = mesh[1];
     auto xes = torch::cat({xv.reshape({-1, 1}), yv.reshape({-1, 1})}, 1).view({1, 1, -1, 2});
 
-    cudaDeviceSynchronize();
-    auto rendering = parallel_forward(mixture, xes).cpu().view({n_batch, n_layers, resolution, resolution});
-    cudaDeviceSynchronize();
+    return parallel_forward(mixture, xes).cpu().view({n_batch, n_layers, resolution, resolution});
+}
+
+void show(torch::Tensor rendering, const int resolution, const int n_batch_limit) {
+    rendering = rendering.clone();
+    const auto n_layers = gpe::n_layers(rendering);
+    const auto n_batch = std::min(gpe::n_batch(rendering), n_batch_limit);
     rendering -= rendering.min();
     rendering /= rendering.max();
     rendering *= 255;
@@ -82,8 +86,9 @@ int main(int argc, char *argv[]) {
             if (USE_CUDA)
                 mixture = mixture.cuda();
             std::cout << "layer " << i << ": " << mixture.sizes() << " device: " << mixture.device() << std::endl;
-//            if (RENDER)
-//                show(gpe::mixture_with_inversed_covariances(mixture), 128, LIMIT_N_BATCH);
+            auto gt_rendering = render(gpe::mixture_with_inversed_covariances(mixture), 128, LIMIT_N_BATCH);
+            if (RENDER)
+                show(gt_rendering, 128, LIMIT_N_BATCH);
 
             cudaDeviceSynchronize();
 
@@ -92,10 +97,13 @@ int main(int argc, char *argv[]) {
             std::tie(fitted_mixture, nodes, aabbs) = bvh_mhem_fit_forward(mixture, 32);
             cudaDeviceSynchronize();
             auto end = std::chrono::high_resolution_clock::now();
+            auto fitted_rendering = render(gpe::mixture_with_inversed_covariances(fitted_mixture), 128, LIMIT_N_BATCH);
             if (RENDER) {
-                show(gpe::mixture_with_inversed_covariances(fitted_mixture), 128, LIMIT_N_BATCH);
+                show(fitted_rendering, 128, LIMIT_N_BATCH);
             }
-            std::cout << "elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << "ms\n";
+            auto diff = gt_rendering - fitted_rendering;
+            auto rmse = torch::sqrt(torch::mean(diff * diff)).item<float>();
+            std::cout << "elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << "ms, RMSE: " << rmse << std::endl;
         }
     }
 
