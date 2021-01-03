62bd9503ae0c411fb5b8d8235138c69051a88756
Adam Celarek <git_commits@xibo.at>
2021-01-03 13:58:42 +0100

    bvh fit: add real world unit test for gradient

--- src/gmc/cpp/extensions/unittests/working_vs_alpha.cpp
+++ src/gmc/cpp/extensions/unittests/working_vs_alpha.cpp
@@ -15,7 +15,7 @@
 #include "util/mixture.h"
 #include "unittests/support.h"
 
-template <int N_REDUCTION, typename scalar_t>
+template <int N_REDUCTION, typename scalar_t, int N_FITTING_COMPONENTS = N_REDUCTION>
 void runTest(const std::vector<std::pair<torch::Tensor, torch::Tensor>>& testCases, double threshold) {
     using namespace torch::indexing;
 
@@ -25,7 +25,7 @@ void runTest(const std::vector<std::pair<torch::Tensor, torch::Tensor>>& testCas
                                    bvh_mhem_fit::Config::FitInitialDisparityMethod::CentroidDistance,
                                    bvh_mhem_fit::Config::FitInitialClusterMergeMethod::MaxWeight,
                                    1.5f,
-                                   N_REDUCTION};
+                                   N_FITTING_COMPONENTS};
     auto reference_config = bvh_mhem_fit_alpha::Config{
             config.reduction_n, config.bvh_config,
             bvh_mhem_fit_alpha::Config::FitInitialDisparityMethod(int(config.fit_initial_disparity_method)),
@@ -36,7 +36,7 @@ void runTest(const std::vector<std::pair<torch::Tensor, torch::Tensor>>& testCas
 
     for (const auto& test_case : testCases) {
         torch::Tensor mixture = test_case.first;
-        torch::Tensor gradient_fitting = test_case.second;
+        torch::Tensor gradient_fitting = test_case.second.repeat({mixture.size(0), mixture.size(1), 1, 1});
         auto reference_mixture = mixture.to(torch::ScalarType::Double);
         auto reference_gradient_fitting = gradient_fitting.to(torch::ScalarType::Double);
         if (sizeof(scalar_t) > 4) {
@@ -153,5 +153,22 @@ TEST_CASE( "testing working against alpha reference", "[bvh_mhem_fit]" ) {
         runTest<4, float>(_combineCollectionsOfGradsAndMixtures({_collectionOf2d4GsGrads()},
                                                                 {_collectionOf2dMixtures_causingNumericalProblems()}),
                           0.002);
+    }
+
+    SECTION("32 component fitting double of real world") {
+        using namespace torch::indexing;
+        std::vector<torch::Tensor> mixtures;
+        for (int i = 0; i < 1/*10*/; ++i) {
+            torch::jit::script::Module container = torch::jit::load("/home/madam/Documents/work/tuw/gmc_net/data/fitting_input/fitting_input_batch" + std::to_string(i) + ".pt");
+
+            for (uint i = 0; i < 3; i++) {
+                auto mixture = container.attr(std::to_string(i)).toTensor();
+                mixture = mixture.index({Slice(0,2), Slice(0,2), Slice(), Slice()});
+                mixture = gpe::pack_mixture(torch::abs(gpe::weights(mixture)), gpe::positions(mixture), gpe::covariances(mixture));
+                mixtures.push_back(mixture);
+            }
+        }
+
+        runTest<4, double, 32>(_combineCollectionsOfGradsAndMixtures({_collectionOf2d32GsGrads()}, {mixtures}), 0.0000000000000000001);
     }
 }
