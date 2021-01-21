fa42aaba79f66da54c99ddb1a8daa0ebb2c7f54b
Adam Celarek <git_commits@xibo.at>
2021-01-21 23:08:15 +0100

    add missing repository to clone script

--- src/gmc/cpp/clone_script.sh
+++ src/gmc/cpp/clone_script.sh
@@ -3,3 +3,4 @@ git clone https://github.com/g-truc/glm.git
 # git clone https://github.com/NVlabs/cub.git
 git clone https://github.com/yohhoy/yamc.git
 git clone https://github.com/autodiff/autodiff.git
+git clone https://github.com/kthohr/gcem.git
