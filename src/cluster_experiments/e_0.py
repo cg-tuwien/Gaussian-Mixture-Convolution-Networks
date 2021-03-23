608c0c7021f380f75da17194a42bbcfbd098e2c2
Adam Ce <adam@dlmain.cluster>
2021-03-23 13:13:08 +0100

    update run config

--- src/cluster_experiments/e_0.py
+++ src/cluster_experiments/e_0.py
@@ -20,9 +20,9 @@ c.log_tensorboard_renderings = False
 c.n_epochs = 160
 
 # network size
-c.layers = [Layer(8, 2.5, 16),
-            Layer(16, 2.5, 8),
-            Layer(32, 2.5, -1)]
+c.layers = [Layer(8, 2.5, 32),
+            Layer(16, 2.5, 16),
+            Layer(128, 2.5, -1)]
 c.mlp = (-1, 40)
 
 main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}bn", config=c)
