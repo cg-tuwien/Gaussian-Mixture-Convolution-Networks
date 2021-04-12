e474a2d8061167c4744568602a7bfe6f59b31c73
Adam Celarek <git_commits@xibo.at>
2021-04-12 18:17:30 +0200

    testing run configs

--- src/qm9/runner.py
+++ src/qm9/runner.py
@@ -12,15 +12,106 @@ c.bn_place = Config.BN_PLACE_NOWHERE
 c.convolution_config.dropout = 0.0
 c.dataDropout = 0.0
 # c.log_tensorboard_renderings = False
-c.n_epochs = 160
-c.training_end_index = 2000
+c.n_epochs = 4
+c.training_end_index = 10000
 c.validation_start_index = 10000
 c.validation_end_index = 10200
+c.batch_size = 50
+c.log_interval = 2500
+c.kernel_learning_rate = 0.005
 
 # network size
-c.layers = [Layer(8, 0.5, 32),
-            Layer(16, 0.5, 16),
-            Layer(1, 0.5, -1)]
-# c.mlp = (-1, 256, -1, 1)
 
-main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}bn", config=c)
+# c.layers = [Layer(8, 0.5, 32),
+#             Layer(16, 0.5, 32),
+#             Layer(32, 0.5, -1)]
+# c.mlp = (-1, 256, -1, 1)
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+# c.layers = [Layer(8, 1.0, 32),
+#             Layer(16, 1.0, 32),
+#             Layer(32, 1.0, -1)]
+# c.mlp = (-1, 256, -1, 1)
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+# c.layers = [Layer(8, 1.5, 32),
+#             Layer(16, 1.5, 32),
+#             Layer(32, 1.5, -1)]
+# c.mlp = (-1, 256, -1, 1)
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+# c.layers = [Layer(8, 0.5, 32),
+#             Layer(16, 0.5, 32),
+#             Layer(32, 0.5, -1)]
+# c.mlp = (-1, 1)
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+c.layers = [Layer(8, 1.0, 32),
+            Layer(16, 1.0, 32),
+            Layer(32, 1.0, -1)]
+c.mlp = (-1, 1)
+main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+
+c.mlp_dropout = 0.0
+c.layers = [Layer(8, 1.0, 32),
+            Layer(16, 1.0, 32),
+            Layer(32, 1.0, -1)]
+c.mlp = (-1, 1)
+main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+
+c.layers = [Layer(8, 1.0, 16),
+            Layer(16, 1.0, 16),
+            Layer(32, 1.0, -1)]
+c.mlp = (-1, 1)
+main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+
+c.layers = [Layer(8, 1.0, 8),
+            Layer(16, 1.0, 8),
+            Layer(32, 1.0, -1)]
+c.mlp = (-1, 1)
+main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+# c.layers = [Layer(8, 1.5, 32),
+#             Layer(16, 1.5, 32),
+#             Layer(32, 1.5, -1)]
+# c.mlp = (-1, 1)
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+# c.layers = [Layer(15, 0.5, 32),
+#             Layer(30, 0.5, 32),
+#             Layer(1, 0.5, -1)]
+# c.mlp = None
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+# c.layers = [Layer(15, 1.0, 32),
+#             Layer(30, 1.0, 32),
+#             Layer(1, 1.0, -1)]
+# c.mlp = None
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+# c.layers = [Layer(15, 1.5, 32),
+#             Layer(30, 1.5, 32),
+#             Layer(1, 1.5, -1)]
+# c.mlp = None
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+# c.layers = [Layer(8, 0.5, 32),
+#             Layer(16, 0.5, 32),
+#             Layer(32, 0.5, 16),
+#             Layer(1, 0.5, -1)]
+# c.mlp = None
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+# c.layers = [Layer(8, 0.75, 32),
+#             Layer(16, 0.75, 32),
+#             Layer(32, 0.75, 16),
+#             Layer(1, 0.75, -1)]
+# c.mlp = None
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
+#
+# c.layers = [Layer(8, 1.0, 32),
+#             Layer(16, 1.0, 32),
+#             Layer(32, 1.0, 16),
+#             Layer(1, 1.0, -1)]
+# c.mlp = None
+# main.experiment(device=device, desc_string=f"qm9_{c.produce_description()}", config=c)
