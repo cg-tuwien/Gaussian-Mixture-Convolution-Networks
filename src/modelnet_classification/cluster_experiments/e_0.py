8c6fcd4c7864fca1338f99e25071dea7b9be9f0b
Adam Celarek <git_commits@xibo.at>
2021-05-01 21:32:55 +0200

    tests with more fitting gaussians.

--- src/modelnet_classification/cluster_experiments/e_0.py
+++ src/modelnet_classification/cluster_experiments/e_0.py
@@ -8,12 +8,13 @@ import cluster_experiments.pc_fit as pcfit
 # device = list(sys.argv)[1]
 device = "cuda"
 
-tmp_gmm_base_path = "/scratch/acelarek/gmms/e0"
+# tmp_gmm_base_path = "/scratch/acelarek/gmms/e0"
+tmp_gmm_base_path = None
 
 fitconf = pcfit.Config(n_gaussians=128, eps=0.00001, gengmm_path=tmp_gmm_base_path)
-pcfit.fit(fitconf)
+# pcfit.fit(fitconf)
 
-c: Config = Config(gmms_fitting=fitconf.name, gengmm_path=tmp_gmm_base_path, n_classes=40)
+c: Config = Config(gmms_fitting=fitconf.name, gengmm_path=tmp_gmm_base_path, n_classes=10)
 c.model.bn_type = ModelConfig.BN_TYPE_COVARIANCE_STD
 c.model.convolution_config.dropout = 0.0
 c.model.dataDropout = 0.0
@@ -21,10 +22,10 @@ c.log_tensorboard_renderings = False
 c.n_epochs = 160
 
 # network size
-c.model.layers = [Layer(12, 2.5, 48),
-                  Layer(24, 2.5, 24),
-                  Layer(48, 2.5, 12),
-                  Layer(40, 2.5, -1)]
+c.model.layers = [Layer(8, 2.5, 128),
+                  Layer(16, 2.5, 64),
+                  Layer(32, 2.5, 32),
+                  Layer(10, 2.5, -1)]
 # c.model.mlp = (-1, 40)
 
 main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c)
--- src/modelnet_classification/cluster_experiments/e_2.py
+++ src/modelnet_classification/cluster_experiments/e_2.py
@@ -8,23 +8,24 @@ import cluster_experiments.pc_fit as pcfit
 # device = list(sys.argv)[1]
 device = "cuda"
 
-tmp_gmm_base_path = "/scratch/acelarek/gmms/e2"
+# tmp_gmm_base_path = "/scratch/acelarek/gmms/e0"
+tmp_gmm_base_path = None
 
 fitconf = pcfit.Config(n_gaussians=128, eps=0.00001, gengmm_path=tmp_gmm_base_path)
-pcfit.fit(fitconf)
+# pcfit.fit(fitconf)
 
-c: Config = Config(gmms_fitting=fitconf.name, gengmm_path=tmp_gmm_base_path, n_classes=40)
-c.model.bn_type = ModelConfig.BN_TYPE_COVARIANCE_STD
+c: Config = Config(gmms_fitting=fitconf.name, gengmm_path=tmp_gmm_base_path, n_classes=10)
+c.model.bn_type = ModelConfig.BN_TYPE_COVARIANCE
 c.model.convolution_config.dropout = 0.0
 c.model.dataDropout = 0.0
 c.log_tensorboard_renderings = False
 c.n_epochs = 160
 
 # network size
-c.model.layers = [Layer(8, 2.5, 64),
-                  Layer(16, 2.5, 32),
-                  Layer(32, 2.5, 16),
-                  Layer(40, 2.5, -1)]
+c.model.layers = [Layer(8, 2.5, 128),
+                  Layer(16, 2.5, 64),
+                  Layer(32, 2.5, 32),
+                  Layer(10, 2.5, -1)]
 # c.model.mlp = (-1, 40)
 
 main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c)
