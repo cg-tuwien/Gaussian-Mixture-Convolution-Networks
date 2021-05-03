this seems to fix the singular covariance issue during learning for me. simon, please verify!

src/pcfitting/generators/eckart_generator_sp.py
@@ -569,7 +569,7 @@ class EckartGeneratorSP(GMMGenerator):
         # To avoid NaNs, we will then replace those invalid values with 0 (pos) and eps (cov).
         nans = torch.isnan(gm_data.priors) | (gm_data.priors == 0)
         gm_data.positions[nans] = torch.tensor([0.0, 0.0, 0.0], dtype=self._dtype, device=general_config.device)
-        gm_data.covariances[nans] = eps[0, 0, 0, :, :]
+        gm_data.covariances[nans] = torch.eye(3, dtype=self._dtype, device=general_config.device)
         gm_data.priors[nans] = 0
 
     @staticmethod