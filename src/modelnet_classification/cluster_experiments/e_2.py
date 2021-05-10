94a84f72f4aebb7480165de05baa1ce0f72fa8d0
Adam Celarek <git_commits@xibo.at>
2021-05-10 14:07:06 +0200

    test

--- src/modelnet_classification/cluster_experiments/e_2.py
+++ src/modelnet_classification/cluster_experiments/e_2.py
@@ -10,15 +10,29 @@ device = "cuda"
 
 tmp_gmm_base_path = None
 
-fitconf = pcfit.Config(n_gaussians=64, eps=0.00001, gengmm_path=tmp_gmm_base_path)
+fitconf = pcfit.Config(n_gaussians=128, eps=0.00001, gengmm_path=tmp_gmm_base_path)
 # pcfit.fit(fitconf)
 
 c: Config = Config(gmms_fitting=fitconf.name, gengmm_path=tmp_gmm_base_path, n_classes=10)
 c.model.bn_type = ModelConfig.BN_TYPE_COVARIANCE
 c.log_tensorboard_renderings = False
 c.model.dropout = 0.0
+# c.batch_size = 10
 
-c.model.layers = [Layer(8, 2.5, 32),
-                  Layer(16, 2.5, 16),
+c.model.layers = [Layer(10, 2.5, -1)]
+main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length2")
+
+c.model.layers = [Layer(8, 2.5, 64),
                   Layer(10, 2.5, -1)]
-main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length")
+main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length2")
+
+c.model.layers = [Layer(8, 2.5, 64),
+                  Layer(16, 2.5, 32),
+                  Layer(10, 2.5, -1)]
+main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length2")
+
+c.model.layers = [Layer(8, 2.5, 64),
+                  Layer(16, 2.5, 32),
+                  Layer(32, 2.5, 16),
+                  Layer(10, 2.5, -1)]
+main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length2")
