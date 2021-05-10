4932cef60d58ee59cddf8d502901b6a57bd8c0df
Adam Celarek <git_commits@xibo.at>
2021-05-10 21:05:04 +0200

    test with treehem 2

--- src/modelnet_classification/cluster_experiments/e_2.py
+++ src/modelnet_classification/cluster_experiments/e_2.py
@@ -2,6 +2,7 @@ import sys
 import modelnet_classification.main as main
 from modelnet_classification.config import Config
 from gmc.model import Layer, Config as ModelConfig
+import gmc.fitting as fitting
 
 import cluster_experiments.pc_fit as pcfit
 
@@ -15,24 +16,25 @@ fitconf = pcfit.Config(n_gaussians=128, eps=0.00001, gengmm_path=tmp_gmm_base_pa
 
 c: Config = Config(gmms_fitting=fitconf.name, gengmm_path=tmp_gmm_base_path, n_classes=10)
 c.model.bn_type = ModelConfig.BN_TYPE_COVARIANCE
+c.model.relu_config.fitting_method = fitting.fixed_point_and_tree_hem2
 c.log_tensorboard_renderings = False
 c.model.dropout = 0.0
-c.batch_size = 10
+c.batch_size = 14
 
 c.model.layers = [Layer(10, 2.5, -1)]
-main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length2")
+main.experiment(device=device, desc_string=f"trhm2_{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length3")
 
 c.model.layers = [Layer(8, 2.5, 64),
                   Layer(10, 2.5, -1)]
-main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length2")
+main.experiment(device=device, desc_string=f"trhm2_{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length3")
 
 c.model.layers = [Layer(8, 2.5, 64),
                   Layer(16, 2.5, 32),
                   Layer(10, 2.5, -1)]
-main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length2")
+main.experiment(device=device, desc_string=f"trhm2_{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length3")
 
 c.model.layers = [Layer(8, 2.5, 64),
                   Layer(16, 2.5, 32),
                   Layer(32, 2.5, 16),
                   Layer(10, 2.5, -1)]
-main.experiment(device=device, desc_string=f"{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length2")
+main.experiment(device=device, desc_string=f"trhm2_{fitconf.name}_{c.produce_description()}", config=c, ablation_name="network_length3")
