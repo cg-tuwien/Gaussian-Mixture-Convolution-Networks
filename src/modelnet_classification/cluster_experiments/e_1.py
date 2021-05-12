21ad45eff2bf6afb2792836e3720540b8f72f2c5
Adam Celarek <git_commits@xibo.at>
2021-05-12 09:35:35 +0200

    next attempt at modelnet40

--- src/modelnet_classification/cluster_experiments/e_1.py
+++ src/modelnet_classification/cluster_experiments/e_1.py
@@ -4,17 +4,24 @@ from modelnet_classification.config import Config
 from gmc.model import Layer, Config as ModelConfig
 import gmc.fitting as fitting
 
+from pcfitting import MaxIterationTerminationCriterion, RelChangeTerminationCriterion
+from pcfitting.generators import EMGenerator, PreinerGenerator, EckartGeneratorSP
 import cluster_experiments.pc_fit as pcfit
 
 # device = list(sys.argv)[1]
 device = "cuda"
 
+# tmp_gmm_base_path = "/scratch/acelarek/gmms/"
 tmp_gmm_base_path = None
 
-fitconf = pcfit.Config(n_gaussians=128, eps=0.00001, gengmm_path=tmp_gmm_base_path)
-# pcfit.fit(fitconf)
+n_g = 128
+fitting_name = f"init-5-{n_g}"
+# pcfit.run(fitting_name,
+#           EMGenerator(n_gaussians=n_g, initialization_method="fpsmax", termination_criterion=MaxIterationTerminationCriterion(0), em_step_points_subbatchsize=10000, eps=1e-5),
+#           gengmm_path=tmp_gmm_base_path,
+#           batch_size=25)
 
-c: Config = Config(gmms_fitting=fitconf.name, gengmm_path=tmp_gmm_base_path, n_classes=40)
+c: Config = Config(gmms_fitting=fitting_name, gengmm_path=tmp_gmm_base_path, n_classes=40)
 c.model.bn_type = ModelConfig.BN_TYPE_COVARIANCE
 c.model.relu_config.fitting_method = fitting.fixed_point_and_tree_hem2
 c.log_tensorboard_renderings = False
@@ -27,4 +34,4 @@ c.model.layers = [Layer(8, 2.5, 64),
                   Layer(64, 2.5, 8),
                   Layer(40, 2.5, -1)]
 
-main.experiment(device=device, desc_string=f"bs14_{fitconf.name}_{c.produce_description()}", config=c, ablation_name="modelnet40")
+main.experiment(device=device, desc_string=f"bs{c.batch_size}_{fitting_name}_{c.produce_description()}", config=c, ablation_name="modelnet40")
