0c1899b9074666aacfc2b0d212a7959b808a0e79
Adam Celarek <git_commits@xibo.at>
2021-05-18 13:22:34 +0200

    use fewer machines on last mnis test

--- src/mnist_classification/cluster_experiments/e_1.py
+++ src/mnist_classification/cluster_experiments/e_1.py
@@ -12,12 +12,16 @@ from mnist_classification.config import Config
 device = "cuda"
 
 c: Config = Config()
-c.input_fitting_iterations = 100
+c.input_fitting_iterations = 1
 c.input_fitting_components = 64
 c.n_epochs = 32
 c.batch_size = 40
 c.log_interval = 1000
+c.model.relu_config.fitting_method = gmc.fitting.fixed_point_and_mhem
 
-# network size
-c.model.layers = [Layer(10, 2.5, -1)]
-main.experiment(device=device, desc_string=f"{c.produce_description()}", config=c, ablation_name="mnist_network_length_correct_norm")
+c.model.layers = [Layer(8, 1.5, 32),
+                  Layer(16, 2.0, 16),
+                  Layer(32, 2.0, 8),
+                  Layer(10, 2.5, -1)]
+main.experiment(device=device, desc_string=f"mem_{c.produce_description()}", config=c, ablation_name="mnist_m2m_fitting")
+
