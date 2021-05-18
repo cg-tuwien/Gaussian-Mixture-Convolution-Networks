3e20381be0cabd847091858df1d8140ba17f826d
Adam Celarek <git_commits@xibo.at>
2021-05-18 13:47:18 +0200

    forgot removing an experiment

--- src/mnist_classification/cluster_experiments/e_3.py
+++ src/mnist_classification/cluster_experiments/e_3.py
@@ -18,11 +18,6 @@ c.n_epochs = 32
 c.batch_size = 40
 c.log_interval = 1000
 
-# network size
-c.model.layers = [Layer(8, 1.5, 32),
-                  Layer(10, 2.5, -1)]
-main.experiment(device=device, desc_string=f"{c.produce_description()}", config=c, ablation_name="mnist_network_length_correct_norm")
-
 c.model.layers = [Layer(8, 1.5, 32),
                   Layer(16, 2.0, 16),
                   Layer(10, 2.5, -1)]
