79da9b14085aefcfad37a16fdd589fc13b1a0014
Adam Celarek <git_commits@xibo.at>
2021-05-19 11:15:37 +0200

    m2m fitting for modelnet, larger network 2

--- src/modelnet_classification/cluster_experiments/e_9.py
+++ src/modelnet_classification/cluster_experiments/e_9.py
@@ -11,7 +11,7 @@ device = "cuda"
 
 tmp_gmm_base_path = None
 
-fitconf = pcfit.Config(n_gaussians=64, eps=0.00001, gengmm_path=tmp_gmm_base_path)
+fitconf = pcfit.Config(n_gaussians=128, eps=0.00001, gengmm_path=tmp_gmm_base_path)
 # pcfit.fit(fitconf)
 
 c: Config = Config(gmms_fitting=fitconf.name, gengmm_path=tmp_gmm_base_path, n_classes=10)
@@ -20,9 +20,9 @@ c.model.relu_config.fitting_method = fitting.fixed_point_and_tree_hem2
 c.log_tensorboard_renderings = False
 c.model.dropout = 0.0
 
-c.model.layers = [Layer(8, 2.5, 32),
-                  Layer(16, 2.5, 16),
-                  Layer(32, 2.5, 8),
-                  Layer(64, 2.5, 4),
+c.model.layers = [Layer(8, 2.5, 64),
+                  Layer(16, 2.5, 32),
+                  Layer(32, 2.5, 16),
+                  Layer(64, 2.5, 8),
                   Layer(10, 2.5, -1)]
 main.experiment(device=device, desc_string=f"treehem2_{fitconf.name}_{c.produce_description()}", config=c, ablation_name="fitting_quality")
