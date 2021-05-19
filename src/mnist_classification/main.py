2359e73994d65dc11a2a7fffce2602da2b4ae26b
Adam Celarek <git_commits@xibo.at>
2021-05-19 11:26:00 +0200

    forgot to log detailed fitting quality for mnist

--- src/mnist_classification/main.py
+++ src/mnist_classification/main.py
@@ -157,9 +157,10 @@ def test(model: gmc.model.Net, device: str, test_loader: torch.utils.data.DataLo
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for batch_id, (data, target) in enumerate(test_loader):
             data, target = data.to(device), target.to(device)
-            output = model(data)
+            step = epoch * len(test_loader) + (batch_id + 1)
+            output = model(data, (tensor_board_writer, step))
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
