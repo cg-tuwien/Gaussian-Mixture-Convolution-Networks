e92cd2b26290de7490ef999841fd9e62efd73a97
Adam Celarek <git_commits@xibo.at>
2021-09-03 17:36:48 +0200

    update dependent libs

--- src/gmc/cpp/clone_script.sh
+++ src/gmc/cpp/clone_script.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/g-truc/glm.git
+git clone git@github.com:adam-ce/glm.git
 # git clone https://github.com/NVlabs/cub.git
 git clone https://github.com/yohhoy/yamc.git
-git clone https://github.com/autodiff/autodiff.git
+git clone git@github.com:adam-ce/autodiff.git
 git clone https://github.com/kthohr/gcem.git
